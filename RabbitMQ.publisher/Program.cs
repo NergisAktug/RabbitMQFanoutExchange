4de382d5d8066218c962688b0eb3084b01c0a6ff
nergis aktuğ <nergis.aktug2014@gmail.com>
2022-01-27 15:22:02 +0300

    direct-queue-Critical

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,32 +1,64 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
+using System;
 
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://mrltcrpj:nG85peo0qknQoavrqwK7OJ3NOB9GTr4T@baboon.rmq.cloudamqp.com/mrltcrpj");
-
-
-using var connection = factory.CreateConnection();
-
-//rabbitMq'ya bir kanal uzerinden baglanilir.
-var channel = connection.CreateModel();
-
-//durable :true demek ExchangeDeclare fiziksel olarak (belleğe) kayıt edilsin demek.
-channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);
-
-Enumerable.Range(1, 50).ToList().ForEach(x =>
+namespace RabbitMQ.publisher
 {
-    string message = $"Message {x}";
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
 
-    //mesaj byte cevriliyor.Turkce karakterlerde sorun yasamamak icin
-    var messageBody = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
-    Console.WriteLine($"Sending messages: {message}");
+    }
 
-});
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://mrltcrpj:nG85peo0qknQoavrqwK7OJ3NOB9GTr4T@baboon.rmq.cloudamqp.com/mrltcrpj");
 
 
+            using var connection = factory.CreateConnection();
+            //rabbitMq'ya bir kanal uzerinden baglanilir.
+            var channel = connection.CreateModel();
+            //durable :true demek ExchangeDeclare fiziksel olarak (belleğe) kayıt edilsin demek.
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Fanout);
+
+            //** Herbir Enum(queue message) foreach ile isimine gore ve direct exchange ile gonderiliyor. 
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var rootKey = $"root-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, durable:true,false,false);
+                channel.QueueBind(queueName, "logs-direct",rootKey);
+            });
 
 
+            //rootlama islemi
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log=(LogNames)new Random().Next(1,5);
+                string message = $"Log-type: {log}";
+
+                //mesaj byte cevriliyor.Turkce karakterlerde sorun yasamamak icin
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                var rootKey = $"root-{log}";
+
+                channel.BasicPublish("logs-direct",rootKey, null, messageBody);
+                Console.WriteLine($"Sending Log: {message}");
+
+            });
 
 
-Console.ReadKey();
+            Console.ReadKey();
+        }
+    } 
+}
+
+
+    
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -11,32 +11,21 @@ using var connection = factory.CreateConnection();
 //rabbitMq'ya bir kanal uzerinden baglanilir.
 var channel = connection.CreateModel();
 
-
-var randomQueueName = channel.QueueDeclare().QueueName;//Kanal uzerindeki queue'lardan random bir kuyruk ismi  verir.
-//channel.QueueDeclare(randomQueueName, true, false, false);channel uzerinde QueueDeclare edersek ilgili subscriber(ilgili instance) oluşsa dahi bu kuyruk durur 
-
-channel.QueueBind(randomQueueName, "logs-fanout","",null);//QueueBind ile bearber uygulama her ayaga kalktıgında bir kuyruk oluşacak ama uygulama down oldugunda uygulama silinecek.    root kısmı şuanda boş
-
-
 //her bir subscriber'a kac mesaj gelecek bunu ayarlamak icin:
 channel.BasicQos(0, 1, false);//global parametresinin true olması tek bir seferde subscriberlara gonderilcek toplam queue sayısı doğrular:Yani 5 queue gonderilecekce bunun 2 Tanesi a sbuscribera gonderiri 3'sunu B subscriber'a gonderir.
 //BasicQos(0,5,false) global'ı false olması tek seferde A subscriber ve B subscriber'lara 5 er queue gonderir.
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume(randomQueueName, false, consumer);//Bir kuyruk ismi istiyor.Bir sonraki parametre autoAck mesaj ulastıktan sonra silinmesi isteniyorsa true isaretlenir.
-
-
+var queueName = "direct-queue-Critical";
+channel.BasicConsume(queueName, false, consumer);//Bir kuyruk ismi istiyor.Bir sonraki parametre autoAck mesaj ulastıktan sonra silinmesi isteniyorsa true isaretlenir.
 Console.WriteLine("Listening to logs...");
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
     Thread.Sleep(1500);
-
     Console.WriteLine("Incoming messages:" + message);
+    //File.AppendAllText("log-critical.txt",message+"\n");
     //DeliveryTag ile bana ulasılan şu taglı mesajı RabbitMQ'ya gonderiyorum,RabbitMq'da ilgili mesajı kuruktan siliyor.
     channel.BasicAck(e.DeliveryTag, false);//multiple parametresi true yapılırsa bunun gibi baska işlenmiş mesajlar var ise memory de onlarıda kuyruktan silmeye yarar.
 };
-
-
-
 Console.ReadKey();
